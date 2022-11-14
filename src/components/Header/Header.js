824659e69697221d4ae2a44150c40aaf9529a961
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-13 21:46:22 -0600

    header added & rendered

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div className='main-header'
+            style={{
+                textAlign: 'center',
+                fontSize: '2em',
+                backgroundColor: '#141414',
+                color: 'white',
+                padding: '1em'
+            }}
+        >
+            Game of Thrones
+        </div>
+    )
+}
+
+export default Header
