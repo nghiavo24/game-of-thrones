cd6d528b872f24241bfbda14e5954bac2ec7f5b8
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-14 10:06:13 -0600

    added API call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar'
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+const[housesDetails, setHousesDetails] = useState([])
+
+  const getHousesDetails = () => {
+  axios
+  .get('https://api.gameofthronesquotes.xyz/v1/houses')
+  .then(res => 
+    setHousesDetails(res.data))
+  .catch((err) =>{
+    console.log(err)}) 
+  }
+  
   return (
     <div>
       <Header />
