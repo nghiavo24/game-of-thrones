37dcde0d3729508db854f6ef36cde3fe8156ec9f
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-15 08:20:17 -0600

    Changed order of Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,10 +7,11 @@ const Header = ({getHousesDetails}) => {
         <div className='main-header'>
             <Link to='/'><div className="first-header">Game of Thrones</div></Link>
             <div className='nav-bar'>
-                <Link to='/houses' ><div className="nav-btn">Houses</div></Link>
-                <Link to='/characters' ><div className="nav-btn">Characters</div></Link>
-                <Link to='/episodes' ><div className="nav-btn">Episodes</div></Link>
                 <Link to='/about' ><div className="nav-btn">About</div></Link>
+                <Link to='/episodes' ><div className="nav-btn">Episodes</div></Link>
+                <Link to='/characters' ><div className="nav-btn">Characters</div></Link>
+                <Link to='/houses' ><div className="nav-btn">Houses</div></Link>
+                
                 <div className="nav-btn">Search</div>
             </div>
         </div>
