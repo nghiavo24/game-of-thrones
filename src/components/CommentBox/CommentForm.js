51ca4766c8992a161dc8d8b6ed86ac8fe76ea28b
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-16 14:10:15 -0600

    setting up CommentForm

--- src/components/CommentBox/CommentForm.js
+++ src/components/CommentBox/CommentForm.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 const CommentForm = () => {
-    return(
-        <div>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
-        </div>
+    return(
+        <form className='comment-form'>
+            <div className="comment-form-fields">
+                <input placeholder="Your Name" required ref={} ></input><br />
+                <textarea placeholder="Leave a comment" rows="4" required ref={}></textarea>
+            </div>
+            <div className="comment-form-actions">
+                <button type="Submit">Post Comment</button>
+            </div>
+        </form>
     )
 }
 
