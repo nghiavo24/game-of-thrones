e8ef28798a7d7f856bd975f0d6306a97b729e6c0
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-15 21:42:59 -0600

    Quotes component worksssssssssgit add .! Yip Yip hooray

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -6,28 +6,30 @@ const Quotes = () => {
     const[randomQuote, setRandomQuote] = useState();
 
     const getRandomQuote = () => {
-        useEffect = (() =>{
-            axios
-        .get('https://api.gameofthronesquotes.xyz/v1/random')
-        .then(res => setRandomQuote(res.data))
-    }, [])
+    axios
+    .get('https://api.gameofthronesquotes.xyz/v1/random')
+    .then(res => setRandomQuote(res.data))
     }
 
-    console.log(randomQuote)
+    useEffect((onClick) =>{
+        getRandomQuote()
+    }, [])
+    
 
     const handleClick = (event) =>{
         event.preventDefault();
         getRandomQuote();
     }
-    
-    if(randomQuote.character);
-    
+    console.log(randomQuote)
+    if(randomQuote)
     return(
         <div>
             <button onClick={handleClick}>Get Random Quote</button>
             <div className='random-quote-container'>
-                <img></img>
-                <h3>by {randomQuote.character}</h3>
+                <img alt=''></img>
+                <h3>'{randomQuote.sentence}'</h3>
+                <h4>by {randomQuote.character.name}</h4>
+                
             </div>
         </div>
     )
