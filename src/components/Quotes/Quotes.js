da903da9b9a4f1c9a0f0af1b267d315c3e8d294c
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-15 20:37:56 -0600

    Boilplate for Quotes component

---
+++ src/components/Quotes/Quotes.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import './Quotes.css'
+import axios from "axios";
+
+const Quotes = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Quotes
