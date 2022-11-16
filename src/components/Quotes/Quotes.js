d97144005e654adc0defb49afec7e621c59bfb4d
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-15 20:46:41 -0600

    API called and rendered successfully

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -3,9 +3,27 @@ import './Quotes.css'
 import axios from "axios";
 
 const Quotes = () => {
+    const[randomQuote, setRandomQuote] = useState();
+
+    const getRandomQuote = () => {
+        axios
+        .get('https://api.gameofthronesquotes.xyz/v1/random')
+        .then(res => setRandomQuote(res.data))
+    }
+    
+    console.log(randomQuote)
+
+    const handleClick = (event) =>{
+        event.preventDefault();
+        getRandomQuote();
+    }
+
     return(
         <div>
+            <button onClick={handleClick}>Get Random Quote</button>
+            <div className='random-quote-container'>
 
+            </div>
         </div>
     )
 }
