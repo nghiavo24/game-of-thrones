ad849aab025b5dad7bef9d127993de04ea0e72a9
nghiavo24 <nghia.vo1452@gmail.com>
2022-11-17 15:41:19 -0600

    fixed background to cover entire webpage

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import backgroundImg from '../../images/background-img.jpg'
+import './About.css'
+
 
 const About = () =>{
     const[aboutTheShow, setAboutTheShow] = useState([]) 
-    
+    const summary ="Based on the bestselling book series A Song of Ice and Fire by George R.R. Martin, this sprawling new HBO drama is set in a world where summers span decades and winters can last a lifetime. From the scheming south and the savage eastern lands, to the frozen north and ancient Wall that protects the realm from the mysterious darkness beyond, the powerful families of the Seven Kingdoms are locked in a battle for the Iron Throne. This is a story of duplicity and treachery, nobility and honor, conquest and triumph. In the Game of Thrones, you either win or you die."
     useEffect(() => {
         const showURL='https://api.tvmaze.com/shows/82'
         axios
@@ -11,16 +14,15 @@ const About = () =>{
         .then(res => setAboutTheShow(res.data))
     }, [])
     
-    // console.log(aboutTheShow)
-
     if(aboutTheShow.image) // conditional rendering
 
     return(
-        <div>
-            <div>
-                <img style={{ width: '60vw', height: '60vh'}} src={aboutTheShow.image.original} alt=''></img>
+        <div 
+        className='about-page-container'>
+            <div className='about-content'>
+                <img className='poster-img' src={aboutTheShow.image.original} alt=''></img>
                 <h1>{aboutTheShow.name}</h1>
-                <h4>Summary: {aboutTheShow.summary}</h4>
+                <h4 className="about-summary">Summary: {summary}</h4>
                 <h4>Premiered: {aboutTheShow.premiered}</h4>
                 <h4>Ended: {aboutTheShow.ended}</h4>
                 <h4>Official Site: {aboutTheShow.officialSite}</h4>
